3e604fb6df69ba412f9abec1e4c8803272058c5c
MJ <47497641+MJtuks@users.noreply.github.com>
2019-11-15 08:27:09 +0200

    More specific plot code
    + Added 2 lines for each car, which represent the front and back positions of the vehicles with respect to time.
    + Added code that fills in the area between each vehicle's lines.
    + Added opacity to the plots so that it is clear when lines overlap (red+blue = purple).
    + Added a y-limit to the plot so that the zone where collisions are a concern can be inspected easily.

--- VectorPlot.py
+++ VectorPlot.py
@@ -1,12 +1,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-vehicle1 = (3, 0.1) #(pi1=initialPosition, v1=calculatedVelocity)
+halfCarLength = 0.2
+vehicle1 = (3, 0.075) #(pi1=initialPosition, v1=calculatedVelocity)
 vehicle2 = (0, 0.2) #(pi1=initialPosition, v1=calculatedVelocity)
 
 time = np.arange(0, 40, 1)
-vehicle1Position = time*vehicle1[1] + vehicle1[0]
-vehicle2Position = time*vehicle2[1] + vehicle2[0]
 
-plt.plot(time, vehicle1Position)
-plt.plot(time, vehicle2Position)
+#position of front of vehicle with respect to time
+vehicle1Position_1 = time*vehicle1[1] + vehicle1[0]+halfCarLength
+vehicle2Position_1 = time*vehicle2[1] + vehicle2[0]+halfCarLength
+
+#position of back of vehicle with respect to time
+vehicle1Position_2 = time*vehicle1[1] + vehicle1[0]-halfCarLength
+vehicle2Position_2 = time*vehicle2[1] + vehicle2[0]-halfCarLength
+
+#collideTime = (vehicle2[0]-vehicle1[0])/(vehicle1[1]-vehicle2[1])
+#collidePosition = vehicle1[1]*collideTime + vehicle1[0]
+
+#plotting the upper and lower position line for every vehicle
+plt.plot(time, vehicle1Position_1, color='b', alpha=0.1)
+plt.plot(time, vehicle2Position_1, color='r', alpha=0.1)
+plt.plot(time, vehicle1Position_2, color='b', alpha=0.1)
+plt.plot(time, vehicle2Position_2, color='r', alpha=0.1)
+
+#filling the area between the two lines for each vehicle
+plt.fill_between(time, vehicle1Position_1, vehicle1Position_2, alpha=0.4, color='b')
+plt.fill_between(time, vehicle2Position_1, vehicle2Position_2, alpha=0.4, color='r')
+
+#limiting the plot the the collision zone (where the vehicles should not collide)
+plt.ylim(4.75,5.25)
+
+plt.title("Collision Zone")
+plt.xlabel("Time (s)")
+plt.ylabel("Position (m)")
+
+#print(collidePosition)
